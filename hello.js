067f0c7209f0d3be0a6a3bb3bee015bdaaa7e28d
MohammedTsmu <mohsat69@yahoo.com>
2024-05-05 10:13:17 +0300

    done

--- hello.js
+++ hello.js
@@ -1,5 +1,3 @@
-let users = ['user1', 'user2', 'user3'];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
 
-let [a, b, c] = users;
-
-console.log(a, b, c);
+console.log(Math.max(...numbers));
