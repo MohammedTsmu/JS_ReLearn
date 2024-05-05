13c846b5e5333521bc7607cd3cde3b15e46d9d06
MohammedTsmu <mohsat69@yahoo.com>
2024-05-05 16:39:34 +0300

    done

--- hello.js
+++ hello.js
@@ -1,3 +1,13 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
 
-console.log(Math.max(...numbers));
+    displayInfo() {
+        console.log(`Car ${this.make}, ${this.model}`);
+    }
+}
+
+let car1 = new Car("Toyota", "Corolla");
+console.log(car1.displayInfo());
